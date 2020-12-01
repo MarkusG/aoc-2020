c0aae92edf2d921e0187f7447fd54bc2087f9a1e
Mark Gross <mark.gross2001@gmail.com>
2020-12-01 14:06:48 -0800

    Make day one more rusty

--- src/day_1.rs
+++ src/day_1.rs
@@ -4,23 +4,38 @@ pub fn day_1() {
     let input = fs::read_to_string("input/1.txt").expect("Error reading file");
     let entries = input.split("\n").map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
-    'outer: for x in &entries {
-        for y in &entries {
+
+    if let Some(product) = search_two(&entries) {
+        println!("First two: {}", product);
+    }
+
+    if let Some(product) = search_three(&entries) {
+        println!("First three: {}", product);
+    }
+}
+
+fn search_two(entries: &[i32]) -> Option<i32> {
+    for x in entries {
+        for y in entries {
             if x + y == 2020 {
-                println!("First two: {}", x * y);
-                break 'outer;
+                return Some(x * y);
             }
         }
     }
 
-    'outer: for x in &entries {
-        for y in &entries {
-            for z in &entries {
+    None
+}
+
+fn search_three(entries: &[i32]) -> Option<i32> {
+    for x in entries {
+        for y in entries {
+            for z in entries {
                 if x + y + z == 2020 {
-                    println!("First three: {}", x * y * z);
-                    break 'outer;
+                    return Some(x * y * z);
                 }
             }
         }
     }
+
+    None
 }
